e6eb47e9645410a9dd7e0db45bcb4c256d541964
Sergey <Sergey@HOME-PC>
2021-03-22 19:39:07 +0300

    epam

--- lab_strings/Program.cs
+++ lab_strings/Program.cs
@@ -9,29 +9,40 @@ namespace lab_strings
 {
     class Program
     {
+
         static void Main(string[] args)
         {
             StreamReader infile = new StreamReader("Inlet.txt");
             StreamWriter outfile = new StreamWriter("Outlet.txt");
 
-            string arr = infile.ReadLine();
-            int length = arr.Length;
-            char[] temp = new char[length];
 
-            int j = 0, k = 1;
-            for (int i = length - 1; i >= length / 2; i--)
+            string arr = infile.ReadToEnd();
+            String[] words = arr.Split('\n', '\r', ' ');
+            int lastEl = words.Length;
+
+            for (int s = 0; s < lastEl; s++)
             {
-                temp[j] = arr[i];
-                j += 2;
-            }
-            for (int l = 0; l < (length / 2); l++)
-            {
-                temp[k] = arr[l];
-                k += 2;
+                string tempStr = words[s];
+                int length = tempStr.Length;
+                char[] temp = new char[length];
+
+                int j = 0, k = 1;
+                for (int i = length - 1; i >= length / 2; i--)
+                {
+                    temp[j] = tempStr[i];
+                    j += 2;
+                }
+                for (int l = 0; l < (length / 2); l++)
+                {
+                    temp[k] = tempStr[l];
+                    k += 2;
+                }
+
+                outfile.WriteLine(temp);
             }
 
-            outfile.WriteLine(temp);
             outfile.Close();
         }
+
     }
 }
