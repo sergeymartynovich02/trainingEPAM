926ca5368a8a550094e39da38e477c1f868d083f
Sergey <Sergey@HOME-PC>
2021-03-25 17:43:47 +0300

    epam

---
+++ lab_exceptions/Matrix.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_exceptions
+{
+    class Matrix
+    {
+        // Скрытые поля
+        private int n;
+        private int stlb;
+        private int[,] mass;
+
+        // Создаем конструкторы матрицы
+        public Matrix() { }
+        public int N
+        {
+            get { return n; }
+            set { if (value > 0) n = value; }
+        }
+        public int ST
+        {
+            get { return stlb; }
+            set { if (value > 0) n = value; }
+        }
+
+        // Задаем аксессоры для работы с полями вне класса Matrix
+        public Matrix(int n, int stlb)
+        {
+            this.n = n;
+            this.stlb = stlb;
+            mass = new int[this.n, this.stlb];
+        }
+        public int this[int i, int j]
+        {
+            get
+            {
+                return mass[i, j];
+            }
+            set
+            {
+                mass[i, j] = value;
+            }
+        }
+
+        // Умножение матрицы А на матрицу В
+        public static Matrix umn(Matrix a, Matrix b)
+        {
+            Matrix resMass = new Matrix(a.N, a.ST);
+                for (int i = 0; i < a.N; i++)
+                    for (int j = 0; j < b.ST; j++)
+                        for (int k = 0; k < b.N; k++)
+                            resMass[i, j] += a[i, k] * b[k, j];
+                return resMass;
+   
+            
+            
+            
+
+            
+        }
+
+        // Перегрузка оператора умножения
+        public static Matrix operator *(Matrix a, Matrix b)
+        {
+            return Matrix.umn(a, b);
+        }
+
+
+
+
+        // Метод вычитания одной матрицы из другой
+        public static Matrix razn(Matrix a, Matrix b)
+        {
+            Matrix resMass = new Matrix(a.N, b.ST);
+            for (int i = 0; i < a.N; i++)
+            {
+                for (int j = 0; j < b.ST; j++)
+                {
+                    resMass[i, j] = a[i, j] - b[i, j];
+                }
+            }
+            return resMass;
+        }
+        // Перегрузка оператора вычитания
+        public static Matrix operator -(Matrix a, Matrix b)
+        {
+            return Matrix.razn(a, b);
+        }
+
+        // Метод, который возвращает нулевую матрицу
+        public void GetEmpty(Matrix a)
+        {
+            Matrix resMass = new Matrix(a.N, a.ST);
+            for (int i = 0; i < a.N; i++)
+            {
+                for (int j = 0; j < a.ST; j++)
+                {
+                    resMass[i, j] = 0;
+                }
+            }
+        }
+
+
+        // Метод сложения матрицы
+        public static Matrix Sum(Matrix a, Matrix b)
+        {
+            Matrix resMass = new Matrix(a.N, a.ST);
+            for (int i = 0; i < a.N; i++)
+            {
+                for (int j = 0; j < b.ST; j++)
+                {
+
+                    resMass[i, j] = a[i, j] + b[i, j];
+                }
+            }
+            return resMass;
+        }
+        // Перегрузка сложения
+        public static Matrix operator +(Matrix a, Matrix b)
+        {
+            return Matrix.Sum(a, b);
+        }
+
+        // Деструктор Matrix
+        ~Matrix()
+        {
+            Console.WriteLine("Очистка");
+        }
+
+    }
+}
--- lab_exceptions/Program.cs
+++ lab_exceptions/Program.cs
@@ -6,118 +6,6 @@ using System.Threading.Tasks;
 
 namespace lab_exceptions
 {
-    class Matrix
-    {
-        // Скрытые поля
-        private int n;
-        private int[,] mass;
-
-        // Создаем конструкторы матрицы
-        public Matrix() { }
-        public int N
-        {
-            get { return n; }
-            set { if (value > 0) n = value; }
-        }
-
-        // Задаем аксессоры для работы с полями вне класса Matrix
-        public Matrix(int n)
-        {
-            this.n = n;
-            mass = new int[this.n, this.n];
-        }
-        public int this[int i, int j]
-        {
-            get
-            {
-                return mass[i, j];
-            }
-            set
-            {
-                mass[i, j] = value;
-            }
-        }
-
-        // Умножение матрицы А на матрицу Б
-        public static Matrix umn(Matrix a, Matrix b)
-        {
-            Matrix resMass = new Matrix(a.N);
-            for (int i = 0; i < a.N; i++)
-                for (int j = 0; j < b.N; j++)
-                    for (int k = 0; k < b.N; k++)
-                        resMass[i, j] += a[i, k] * b[k, j];
-
-            return resMass;
-        }
-
-        // Перегрузка оператора умножения
-        public static Matrix operator *(Matrix a, Matrix b)
-        {
-            return Matrix.umn(a, b);
-        }
-
-
-
-
-        // Метод вычитания одной матрицы из другой
-        public static Matrix razn(Matrix a, Matrix b)
-        {
-            Matrix resMass = new Matrix(a.N);
-            for (int i = 0; i < a.N; i++)
-            {
-                for (int j = 0; j < b.N; j++)
-                {
-                    resMass[i, j] = a[i, j] - b[i, j];
-                }
-            }
-            return resMass;
-        }
-        // Перегрузка оператора вычитания
-        public static Matrix operator -(Matrix a, Matrix b)
-        {
-            return Matrix.razn(a, b);
-        }
-
-        // Метод, который возвращает нулевую матрицу
-        public void GetEmpty(Matrix a)
-        {
-            Matrix resMass = new Matrix(a.N);
-            for (int i = 0; i < a.N; i++)
-            {
-                for (int j = 0; j < a.N; j++)
-                {
-                    resMass[i, j] = 0;
-                }
-            }
-        }
-
-
-        // Метод сложения матрицы
-        public static Matrix Sum(Matrix a, Matrix b)
-        {
-            Matrix resMass = new Matrix(a.N);
-            for (int i = 0; i < a.N; i++)
-            {
-                for (int j = 0; j < b.N; j++)
-                {
-                    resMass[i, j] = a[i, j] + b[i, j];
-                }
-            }
-            return resMass;
-        }
-        // Перегрузка сложения
-        public static Matrix operator +(Matrix a, Matrix b)
-        {
-            return Matrix.Sum(a, b);
-        }
-
-        // Деструктор Matrix
-        ~Matrix()
-        {
-            Console.WriteLine("Очистка");
-        }
-
-    }
     class MainProgram
     {
 
@@ -125,21 +13,22 @@ namespace lab_exceptions
         {
             try { 
 
-                Console.WriteLine("Введите размерность матрицы: ");
+                Console.WriteLine("Введите кол-во строк матрицы: ");
                 int nn = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Введите кол-во столбцов матрицы: ");
+                int stlbc = Convert.ToInt32(Console.ReadLine());
+
                 string answer = "";
                 int temp1 = 0;
                 int temp2 = 0;
 
                 // Инициализация матрицы
-                Matrix mass1 = new Matrix(nn);
-                Matrix mass2 = new Matrix(nn);
-                Matrix mass3 = new Matrix(nn);
-                Matrix mass4 = new Matrix(nn);
-                Matrix mass5 = new Matrix(nn);
-                Matrix mass6 = new Matrix(nn);
-                Matrix mass7 = new Matrix(nn);
-                Matrix mass8 = new Matrix(nn);
+                Matrix mass1 = new Matrix(nn, stlbc);
+                Matrix mass2 = new Matrix(nn, stlbc);
+                Matrix mass3 = new Matrix(nn, stlbc);
+                Matrix mass4 = new Matrix(nn, stlbc);
+                Matrix mass5 = new Matrix(nn, stlbc);
+                Matrix mass6 = new Matrix(nn, stlbc);
 
                 // Проверка надобности применения метода GetEmpy()
                 Console.WriteLine("Матрица А должна быть нулевой? y(да)/n(нет) ");
@@ -157,7 +46,7 @@ namespace lab_exceptions
                     Console.WriteLine("Введите матрицу А по одному элементу через ENTER: ");
                     for (int i = 0; i < nn; i++)
                     {
-                        for (int j = 0; j < nn; j++)
+                        for (int j = 0; j < stlbc; j++)
                         {
                             Console.WriteLine("Введите элемент матрицы {0}:{1}", i + 1, j + 1);
                             mass1[i, j] = Convert.ToInt32(Console.ReadLine());
@@ -188,7 +77,7 @@ namespace lab_exceptions
                     Console.WriteLine("Введите матрицу B по одному элементу через ENTER: ");
                     for (int i = 0; i < nn; i++)
                     {
-                        for (int j = 0; j < nn; j++)
+                        for (int j = 0; j < stlbc; j++)
                         {
                             Console.WriteLine("Введите элемент матрицы {0}:{1}", i + 1, j + 1);
                             mass2[i, j] = Convert.ToInt32(Console.ReadLine());
@@ -202,12 +91,12 @@ namespace lab_exceptions
                     Main();
                 }
 
-                if (temp1 == temp2)
+                if (temp1 == 1 && temp2 == 1)
                 {
                     Console.WriteLine("Рассчёты бессмыслены, так как обе матрицы нулевые. \nИ в итоге каждого из них результат будет один и тот же. \nА именно нулевая матрица:  ");
                     for (int i = 0; i < nn; i++)
                     {
-                        for (int j = 0; j < nn; j++)
+                        for (int j = 0; j < stlbc; j++)
                         {
                             Console.Write(mass1[i, j] + "\t");
                         }
@@ -223,7 +112,7 @@ namespace lab_exceptions
                     Console.WriteLine("Матрица А: ");
                     for (int i = 0; i < nn; i++)
                     {
-                        for (int j = 0; j < nn; j++)
+                        for (int j = 0; j < stlbc; j++)
                         {
                             Console.Write(mass1[i, j] + "\t");
                         }
@@ -235,7 +124,7 @@ namespace lab_exceptions
                     Console.WriteLine("Матрица В: ");
                     for (int i = 0; i < nn; i++)
                     {
-                        for (int j = 0; j < nn; j++)
+                        for (int j = 0; j < stlbc; j++)
                         {
                             Console.Write(mass2[i, j] + "\t");
                         }
@@ -245,12 +134,12 @@ namespace lab_exceptions
 
                     //Вывод на экран суммы матриц А и B
                     Console.WriteLine("Сумма матриц А и B: ");
-                    mass4 = (mass1 + mass2);
+                    mass3 = (mass1 + mass2);
                     for (int i = 0; i < nn; i++)
                     {
-                        for (int j = 0; j < nn; j++)
+                        for (int j = 0; j < stlbc; j++)
                         {
-                            Console.Write(mass4[i, j] + "\t");
+                            Console.Write(mass3[i, j] + "\t");
                         }
                         Console.WriteLine();
                     }
@@ -258,12 +147,12 @@ namespace lab_exceptions
 
                     //Вывод на экран разности матриц A и B (A-B)
                     Console.WriteLine("Разность матриц А и B (Матрица А - Матрица В): ");
-                    mass6 = (mass1 - mass2);
+                    mass4 = (mass1 - mass2);
                     for (int i = 0; i < nn; i++)
                     {
-                        for (int j = 0; j < nn; j++)
+                        for (int j = 0; j < stlbc; j++)
                         {
-                            Console.Write(mass6[i, j] + "\t");
+                            Console.Write(mass4[i, j] + "\t");
                         }
                         Console.WriteLine();
                     }
@@ -271,32 +160,39 @@ namespace lab_exceptions
 
                     //Вывод на экран разности матриц B и A (B-A)
                     Console.WriteLine("Разность матриц B и A (Матрица В - Матрица А): ");
-                    mass7 = (mass2 - mass1);
+                    mass5 = (mass2 - mass1);
                     for (int i = 0; i < nn; i++)
                     {
-                        for (int j = 0; j < nn; j++)
+                        for (int j = 0; j < stlbc; j++)
                         {
-                            Console.Write(mass7[i, j] + "\t");
-                        }
-                        Console.WriteLine();
-                    }
-                    Console.WriteLine();
-
-                    //Вывод на экран произведения матриц A и B
-                    Console.WriteLine("Произведение матриц A и B: ");
-                    mass8 = (mass1 * mass2);
-                    for (int i = 0; i < nn; i++)
-                    {
-                        for (int j = 0; j < nn; j++)
-                        {
-                            Console.Write(mass8[i, j] + "\t");
+                            Console.Write(mass5[i, j] + "\t");
                         }
                         Console.WriteLine();
                     }
                     Console.WriteLine();
 
 
-                    Console.ReadKey();
+                    if (nn == stlbc)
+                    {
+                        //Вывод на экран произведения матриц A и B
+                        Console.WriteLine("Произведение матриц A и B: ");
+                        mass6 = (mass1 * mass2);
+                        for (int i = 0; i < nn; i++)
+                        {
+                            for (int j = 0; j < stlbc; j++)
+                            {
+                                Console.Write(mass6[i, j] + "\t");
+                            }
+                            Console.WriteLine();
+                        }
+                        Console.WriteLine();
+                        Console.ReadKey();
+                    }
+                    else
+                    {
+                        Console.WriteLine("Умножение матрицы А на матрицу Б невозможно по следующей причине: \nЧисло столбцов матрицы А совпадает с числом строк матрицы В.");
+                        Console.ReadKey();
+                    }
 
                 }
                 
