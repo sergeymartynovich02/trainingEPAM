192b259b61932a7fa94c3a38916597a170f7f8f0
Sergey <Sergey@HOME-PC>
2021-04-09 18:05:08 +0300

    epam

---
+++ lab_Delegates_task_1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_Delegates_task_1
+{
+
+
+
+
+    /* Func<Action<string,int>, int,int>  */
+    
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Func < Action<string, int>, int, int> lambda = (f, x) =>
+            {
+                return (x == 4) ? (2 * x) : (x * x);
+            };
+            Action<string, int> TT = MyFunc;
+            Console.WriteLine(lambda(TT, 4));
+            Console.ReadKey();
+
+        }
+        static void MyFunc(string f, int x)
+        {
+            Console.WriteLine(f + x + " b c d e f");
+            Console.ReadLine();
+        }
+
+
+    }
+}
