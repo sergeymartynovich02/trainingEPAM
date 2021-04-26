319132d9d50b325a8851ad03562e025f25e9224c
Sergey <Sergey@HOME-PC>
2021-04-27 01:14:13 +0300

    epam

---
+++ lab_XML_Task1/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+    Даны имена существующего текстового файла и создаваемого XML-документа.
+    Каждая строка текстового файла содержит несколько (одно или более) слов, разделенных ровно одним пробелом.
+    Создать XML-документ с корневым элементом root, элементами первого уровня line и элементами второго уровня word.
+    Элементы line соответствуют строкам исходного файла и не содержат дочерних текстовых узлов,
+    элементы word каждого элемента line содержат по одному слову из соответствующей строки 
+    (слова располагаются в порядке их следования в исходной строке). 
+    Элемент line должен содержать атрибут num, равный порядковому номеру строки в исходном файле,
+    элемент word должен содержать атрибут num, равный порядковому номеру слова в строке. 
+*/
+
+
+namespace lab_XML_Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XDocument xdoc = new XDocument();
+            XElement root = new XElement("root");
+
+            StreamReader fileIn = new StreamReader("file.txt");
+            string str; int[] nums = { 0, 0 };
+
+            while ((str = fileIn.ReadLine()) != null)
+            {
+                XElement line = new XElement("line");
+                XAttribute num1 = new XAttribute("num", nums[0].ToString());
+                line.Add(num1);
+
+                string[] s1 = str.Trim().Split(); nums[1] = 0;
+                foreach (string s in s1)
+                {
+                    XElement word = new XElement("word");
+                    XAttribute num2 = new XAttribute("num", nums[1].ToString());
+                    XText text = new XText(s);
+                    word.Add(num2);
+                    word.Add(text);
+                    line.Add(word);
+                    nums[1]++;
+                }
+                root.Add(line);
+                nums[0]++;
+            }
+            fileIn.Close();
+            xdoc.Add(root);
+            xdoc.Save("xmlFile");
+            Console.WriteLine(xdoc + "\n");
+            Console.ReadKey();
+            
+        }
+    }
+}
---
+++ lab_XML_Task2/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Xml;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+using System.Text;
+
+/*
+    Дан XML - документ, содержащий хотя бы один атрибут. Найти и вывести имена атрибутов,
+    а также все связанные с ними значения (все значения считаются текстовыми).
+    Порядок имен должен соответствовать порядку их первого вхождения в документ; 
+    значения, связанные с каждым именем, выводить в алфавитном порядке.
+*/
+
+namespace Lab_XML_Task2
+{
+    class Program
+    {
+        static void Main()
+        {
+            XDocument xDoc = XDocument.Load("XmlFile.xml");
+
+            Console.WriteLine("ИЗНАЧАЛЬНЫЙ XML ФАЙЛ: \n\n" + xDoc + "\n\n");
+
+
+            var res = xDoc.Element("Root")
+                            .Elements()
+                            .OrderBy(c => (string)c.Attribute("Attribute1"));
+
+            XDocument doc = new XDocument(new XElement("Root", res));
+
+
+            Console.WriteLine("КОНЕЧНЫЙ XML ФАЙЛ: \n\n" + doc + "\n\n");
+
+
+
+            doc.Save("file1.xml");
+            XmlDocument xDoc1 = new XmlDocument();
+            xDoc1.Load(Path.GetFullPath("file1.xml"));
+            XmlElement xRoot = xDoc1.DocumentElement;
+            foreach (XmlNode xnode in xRoot)
+            {
+                if (xnode.Attributes.Count > 0)
+                {
+                    XmlNode attr = xnode.Attributes.GetNamedItem("Attribute1");
+                    if (attr != null)
+                        Console.WriteLine("Имя атрибута: " + attr.Name + " ==> Значение: " + attr.InnerText);
+                }
+
+            }
+            Console.WriteLine("\n");
+            Console.ReadKey();
+        }
+    }
+}
